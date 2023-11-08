92991bee937309e72cd9754bb4cf17954bd0e241
Narmin Aghamirzali <nerminagamirzeli501@gmail.com>
2023-11-08 21:51:26 +0400

    layout correcting

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import './css/reset.css';
 import './css/style.scss';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Login from './components/Login';
-import PersonalFiles from './components/PersonalFiles';
-import Profile from './components/Profile';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate replace to="/login" />} />
       <Route path='/login' element={<Login />} />
-      <Route path='/personal-files' element={<PersonalFiles />} />
-      <Route path='/personal-files/:id' element={<PersonalFiles />} />
-      <Route path='/profile' element={<Profile />} />
+      <Route path='/personal-files' element={<Layout />} />
+      <Route path='/personal-files/:id' element={<Layout />} />
     </Routes>
   );
 }
